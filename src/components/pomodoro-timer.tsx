7eaa960a74938cd26f57885f4ae988f8cdfb9508
Leandro Silva <lleandro.adal@gmail.com>
2026-06-02 15:02:01 -0300

    feat: implement Work button functionality

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,6 +1,5 @@
-import React, { JSX } from 'react';
+import React, { JSX, useEffect } from 'react';
 import { useInterval } from '../hooks/use-interval';
-import { secondsToTime } from '../utils/seconds-to-time';
 import { Button } from './buttom';
 import { Timer } from './timer';
 
@@ -12,19 +11,46 @@ interface Props {
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
+  // armazena o tempo atual do contador
   const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
+  // Controla se o cronômetro está rodando ou parado
+  const [timeCounting, setTimeCounting] = React.useState(false);
+  // Indica o status do trabalho
+  const [working, setWorking] = React.useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  // Quando working mudar
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
+
+  // Hook personalizado responsável por executar uma função em intervalos
+  useInterval(
+    () => {
+      // Reduz 1 segundo do tempo atual a cada execução
+      setMainTime(mainTime - 1);
+    },
+    // Se o contador estiver ativo, executa a cada 1000ms (1 segundo)
+    timeCounting ? 1000 : null,
+  );
+
+  const configureWork = () => {
+    // Inicia o contador
+    setTimeCounting(true);
+
+    // Define que estamos em período de trabalho
+    // Muda o estado pro css ser alterado
+    setWorking(true);
+  };
 
   return (
     <div className="pomodoro">
       <h2>You are working</h2>
+
+      {/* Componente responsável por exibir o tempo */}
       <Timer mainTime={mainTime} />
 
       <div className="controls">
-        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text="Work" onClick={() => configureWork()}></Button>
         <Button text="teste" onClick={() => console.log(1)}></Button>
         <Button text="teste" onClick={() => console.log(1)}></Button>
       </div>
